255349bca28f34d6419f4b7c7b10c50d75c783fb
Qin Guan <qinguan20040914@gmail.com>
2023-07-27 15:34:45 +0800

    feat: icons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,22 +4,22 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="dashboard" href="/home/dashboard">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon aria-hidden="true" :icon="homeOutline" />
           <ion-label>Dashboard</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="help" href="/home/help">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+          <ion-icon aria-hidden="true" :icon="handLeftOutline" />
           <ion-label>Help</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="chat" href="/home/chat">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-icon aria-hidden="true" :icon="chatboxOutline" />
           <ion-label>Chat</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="forum" href="/home/forum">
-          <ion-icon aria-hidden="true" :icon="chatbox" />
+          <ion-icon aria-hidden="true" :icon="newspaperOutline" />
           <ion-label>Forum</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -29,5 +29,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle, chatbox} from 'ionicons/icons';
+import { ellipse, square, handLeftOutline, chatbox, home, newspaperOutline, chatboxOutline, homeOutline} from 'ionicons/icons';
 </script>
